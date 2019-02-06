2ff4c301038613b1c7d2e34ac25f2e4911ada82a
Dennis Boon <dennisvdboon@gmail.com>
2019-02-06 21:48:29 +0000

    Removed extraneous MorphOS code.

--- main/SDL_error.c
+++ main/SDL_error.c
@@ -112,68 +112,6 @@ void SDL_SetError (const char *fmt, ...)
 #endif
 }
 
-#if defined(__MORPHOS__)
-void __saveds QSDL_SetError (void *dummy, const char *fmt, ...)
-{
-	va_list ap;
-	SDL_error *error;
-
-	/* Copy in the key, mark error as valid */
-	error = SDL_GetErrBuf();
-	error->error = 1;
-	SDL_strlcpy((char *)error->key, fmt, sizeof(error->key));
-
-	va_start(ap, fmt);
-	error->argc = 0;
-	while ( *fmt ) {
-		if ( *fmt++ == '%' ) {
-			while ( *fmt == '.' || (*fmt >= '0' && *fmt <= '9') ) {
-				++fmt;
-			}
-			switch (*fmt++) {
-			    case 0:  /* Malformed format string.. */
-				--fmt;
-				break;
-			    case 'c':
-			    case 'i':
-			    case 'd':
-			    case 'u':
-			    case 'o':
-			    case 'x':
-			    case 'X':
-				error->args[error->argc++].value_i =
-							va_arg(ap, int);
-				break;
-			    case 'f':
-				error->args[error->argc++].value_f =
-							va_arg(ap, double);
-				break;
-			    case 'p':
-				error->args[error->argc++].value_ptr =
-							va_arg(ap, void *);
-				break;
-			    case 's':
-				{
-				  int i = error->argc;
-				  const char *str = va_arg(ap, const char *);
-				  if (str == NULL)
-				      str = "(null)";
-				  SDL_strlcpy((char *)error->args[i].buf, str, ERR_MAX_STRLEN);
-				  error->argc++;
-				}
-				break;
-			    default:
-				break;
-			}
-			if ( error->argc >= ERR_MAX_ARGS ) {
-				break;
-			}
-		}
-	}
-	va_end(ap);
-}
-#endif
-
 /* This function has a bit more overhead than most error functions
    so that it supports internationalization and thread-safe errors.
 */
