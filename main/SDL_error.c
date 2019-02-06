b0dca2dc0146d9935f687c7169d7c06c7b8c7be5
Dennis Boon <dennisvdboon@gmail.com>
2019-02-06 21:11:40 +0000

    Updated to latest SDL error sources.

--- main/SDL_error.c
+++ main/SDL_error.c
@@ -1,6 +1,6 @@
 /*
     SDL - Simple DirectMedia Layer
-    Copyright (C) 1997-2006 Sam Lantinga
+    Copyright (C) 1997-2012 Sam Lantinga
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -25,9 +25,6 @@
 
 #include "SDL_error.h"
 #include "SDL_error_c.h"
-#include <stdarg.h>
-//#define SDL_arraysize(array)	(sizeof(array)/sizeof(array[0]))
-#define SDL_TABLESIZE(table)	SDL_arraysize(table) 
 
 /* Routine to get the thread-specific error variable */
 #if SDL_THREADS_DISABLED
@@ -114,6 +111,68 @@ void SDL_SetError (const char *fmt, ...)
 	fprintf(stderr, "SDL_SetError: %s\n", SDL_GetError());
 #endif
 }
+
+#if defined(__MORPHOS__)
+void __saveds QSDL_SetError (void *dummy, const char *fmt, ...)
+{
+	va_list ap;
+	SDL_error *error;
+
+	/* Copy in the key, mark error as valid */
+	error = SDL_GetErrBuf();
+	error->error = 1;
+	SDL_strlcpy((char *)error->key, fmt, sizeof(error->key));
+
+	va_start(ap, fmt);
+	error->argc = 0;
+	while ( *fmt ) {
+		if ( *fmt++ == '%' ) {
+			while ( *fmt == '.' || (*fmt >= '0' && *fmt <= '9') ) {
+				++fmt;
+			}
+			switch (*fmt++) {
+			    case 0:  /* Malformed format string.. */
+				--fmt;
+				break;
+			    case 'c':
+			    case 'i':
+			    case 'd':
+			    case 'u':
+			    case 'o':
+			    case 'x':
+			    case 'X':
+				error->args[error->argc++].value_i =
+							va_arg(ap, int);
+				break;
+			    case 'f':
+				error->args[error->argc++].value_f =
+							va_arg(ap, double);
+				break;
+			    case 'p':
+				error->args[error->argc++].value_ptr =
+							va_arg(ap, void *);
+				break;
+			    case 's':
+				{
+				  int i = error->argc;
+				  const char *str = va_arg(ap, const char *);
+				  if (str == NULL)
+				      str = "(null)";
+				  SDL_strlcpy((char *)error->args[i].buf, str, ERR_MAX_STRLEN);
+				  error->argc++;
+				}
+				break;
+			    default:
+				break;
+			}
+			if ( error->argc >= ERR_MAX_ARGS ) {
+				break;
+			}
+		}
+	}
+	va_end(ap);
+}
+#endif
 
 /* This function has a bit more overhead than most error functions
    so that it supports internationalization and thread-safe errors.
